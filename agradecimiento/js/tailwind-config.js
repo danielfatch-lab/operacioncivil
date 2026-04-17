2fcb98d0d45b6747123d9d3bcc00040376d66aad
Daniel Franco <danielfatch@gmail.com>
2026-04-17 19:14:32 -0300

    Actualizando diseño de pagina de gracias

--- agradecimiento/js/tailwind-config.js
+++ agradecimiento/js/tailwind-config.js
@@ -4,15 +4,15 @@ tailwind.config = {
             colors: {
                 brand: {
                     yellow: '#FACC15',
+                    success: '#10B981',
                     borderDark: '#1F1F22',
                     textGray: '#8B8B93',
-                    textWhite: '#E4E4E7',
-                    success: '#10B981', // Verde de confirmación
-                    successDark: '#047857'
+                    textWhite: '#F4F4F5'
                 },
                 dark: {
                     void: '#000000',
-                    surface: '#0A0A0C'
+                    surface: '#030303',
+                    card: '#09090B'
                 }
             },
             fontFamily: {
