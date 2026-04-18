33541b8030851e65ad84da8438c89b838c333ad3
Daniel Franco <danielfatch@gmail.com>
2026-04-18 18:03:39 -0300

    Actualizacion

---
+++ js/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: { 
+                body: ['Inter', 'sans-serif'] 
+            },
+            colors: {
+                brand: { 
+                    black: '#030303',
+                    darkBlue: '#0a0f1a',  
+                    card: '#0d1322',      
+                    borderDark: '#1a233a', 
+                    yellow: '#facc15',    
+                    red: '#DC2626',       
+                    textGray: '#cbd5e1', 
+                    textWhite: '#f8fafc'
+                }
+            }
+        }
+    }
+}
